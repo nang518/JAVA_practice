4d38e514abd2b8fbba53838bc570baa1f1736a75
정나영 <jungnayoung@jeongnayeong-ui-MacBook-Pro.local>
2022-06-05 04:47:06 +0900

    22.06.05

---
+++ src/Circle.java
@@ -0,0 +1,30 @@
+public class Circle {
+    int radius;
+    String name;
+
+    public Circle(){
+        radius = 1;
+        name = "";
+    }
+    public Circle(int r, String n)
+    {
+        radius = r;
+        name = n;
+    }
+    public double getArea()
+    {
+        return 3.14 * radius * radius;
+    }
+
+    public static void main(String[] args)
+    {
+        Circle pizza = new Circle(10, "피자");
+        double area = pizza.getArea();
+        System.out.println(pizza.name+"의 면적은 "+area);
+
+        Circle donut = new Circle();
+        donut.name = "도넛";
+        area = donut.getArea();
+        System.out.println(donut.name+"의 면적은 "+area);
+    }
+}
